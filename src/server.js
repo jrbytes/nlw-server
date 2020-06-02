4c59931eaf9f358d4af3f7eebb37d72d9b19bd8e
Jr Bytes <itamazor@gmail.com>
2020-06-02 18:10:33 -0300

    Implemented initial routes.

--- src/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var app = express_1["default"]();
-app.get('/users', function () {
-    console.log('Hello world');
-});
-app.listen(3333);
